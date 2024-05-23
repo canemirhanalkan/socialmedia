f39916db5131a211502ea466b224a1d79bbde54b
Can Alkan <alkncann@gmail.com>
2024-05-23 20:12:28 +0300

    page layout complated

--- postplatform/views.py
+++ postplatform/views.py
@@ -1,7 +1,38 @@
+import datetime
 from django.shortcuts import render
+import requests
+import locale
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'postplatform/index.html')
+
+#-----------------WEATHER APİ--------------------------------#
+    city = 'Samsun'
+
+    key = '12f9563b5d56a94d23d5c83b004ed158'
+    URL = 'https://api.openweathermap.org/data/2.5/weather'
+    PARAMS = {'q':city, 'appid':key, 'units':'metric', 'lang':'tr'}
+    r = requests.get(url = URL, params=PARAMS)
+    res = r.json()
+    
+    description = res['weather'][0]['description'].upper()
+    icon = res['weather'][0]['icon']
+    temp = res['main']['temp']
+
+    locale.setlocale(locale.LC_ALL,"")
+    day = datetime.date.today().strftime("%A")
+#-----------------WEATHER APİ END-----------------------------#
+
+
+    return render(request, 'postplatform/index.html', {
+        'description':description,
+        'icon':icon,
+        'temp':temp,
+        'day':day,
+        'city':city,
+    })
+
+
+
