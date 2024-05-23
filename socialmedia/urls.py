d318bf1499cf97671a29cdbe8d35c87395f1e45a
Can Alkan <alkncann@gmail.com>
2024-05-23 18:00:29 +0300

    add postplatform link

--- socialmedia/urls.py
+++ socialmedia/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('pages.urls')),
+    path('posts/', include('postplatform.urls')),
     path('admin/', admin.site.urls),
 ]
